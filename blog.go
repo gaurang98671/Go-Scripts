4e9fb283ed03f7b8e104ece2519fa88c8e36f3ea
gaurang98671 <56935661+gaurang98671@users.noreply.github.com>
2021-01-21 17:44:34 +0530

    Post comments endpoint added

--- blog.go
+++ blog.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"strconv"
+
 	//"fmt"
 	"html/template"
 	"log"
@@ -23,10 +26,16 @@ const (
 var database *sql.DB
 
 type Page struct {
-	Title   string
-	Content string
-	Date    string
-	GUIDE   string
+	ID         string
+	Title      string
+	Content    template.HTML
+	RawContent string
+	Date       string
+	GUIDE      string
+}
+
+type JSONResponse struct {
+	Fields map[string]string
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
@@ -44,8 +53,10 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 	defer pages.Close()
 	for pages.Next() {
 		thisPage := Page{}
-		pages.Scan(&thisPage.Title, &thisPage.Content, &thisPage.Date, &thisPage.GUIDE)
-		thisPage.Content = truncate(thisPage)
+		pages.Scan(&thisPage.Title, &thisPage.RawContent, &thisPage.Date, &thisPage.GUIDE)
+
+		thisPage.RawContent = truncate(thisPage)
+		thisPage.Content = template.HTML(thisPage.RawContent)
 		Pages = append(Pages, thisPage)
 	}
 
@@ -60,25 +71,75 @@ func getPage(w http.ResponseWriter, r *http.Request) {
 	guide := vars["guide"]
 	thisPage := Page{}
 	log.Println(guide)
-	err := database.QueryRow("SELECT page_title,page_content,page_date FROM blog WHERE page_guide=?", guide).Scan(&thisPage.Title, &thisPage.Content, &thisPage.Date)
+	err := database.QueryRow("SELECT id,page_title, page_content, page_date FROM blog WHERE page_guide=?", guide).Scan(&thisPage.ID, &thisPage.Title, &thisPage.RawContent, &thisPage.Date)
+	log.Println("Page id is: ", thisPage.ID)
 	if err != nil {
 		log.Println("Page not found")
 		log.Println(err)
 		http.ServeFile(w, r, "404.html")
 	}
-
+	thisPage.GUIDE = guide
+	thisPage.Content = template.HTML(thisPage.RawContent)
 	t, _ := template.ParseFiles("templates/blog.html")
 	t.Execute(w, thisPage)
 }
 
+func APICommentsPost(w http.ResponseWriter, r *http.Request) {
+
+	var apiCommentAdded bool
+	err := r.ParseForm()
+	if err != nil {
+		log.Println(err.Error)
+	}
+
+	name := r.FormValue("name")
+	email := r.FormValue("email")
+	comment := r.FormValue("comment")
+	guide := r.FormValue("guide")
+	page_id := r.FormValue("page_id")
+	log.Println("Name:", name, "Email:", email, "Comment", comment, "guide", guide, "Page id: ", page_id)
+	res, err := database.Exec("INSERT INTO comments(page_id, name, email, comment, comment_guid) VALUES(?,?,?,?,?)", page_id, name, email, comment, guide)
+
+	if err != nil {
+		log.Println("Failed to insert")
+		log.Println(err)
+		fmt.Fprint(w, "Failed to insert comment")
+	} else {
+		log.Println("Inserted")
+		id, err := res.LastInsertId()
+		log.Println(id)
+		log.Println(err)
+		if err != nil {
+			apiCommentAdded = false
+		} else {
+			apiCommentAdded = true
+		}
+
+		//var resp JSONResponse
+
+		commentAddedBool := strconv.FormatBool(apiCommentAdded)
+		log.Println(commentAddedBool)
+		var m map[string]string
+		m = make(map[string]string)
+		m["id"] = string(id)
+		m["added"] = commentAddedBool
+		log.Println(m)
+
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprint(w, m)
+
+	}
+
+}
+
 func truncate(page Page) string {
-	if len(page.Content) > 150 {
+	if len(page.RawContent) > 150 {
 		content := page.Content
 		truncated_content := []rune(content)
 		new_truncated_content := string(truncated_content[0:150]) + "...."
 		return new_truncated_content
 	}
-	return page.Content
+	return page.RawContent
 }
 func main() {
 
@@ -95,5 +156,6 @@ func main() {
 	router.HandleFunc("/", homePage)
 	router.HandleFunc("/page/{guide}", getPage)
 	router.HandleFunc("/home", serveHome)
+	router.HandleFunc("/api/comments", APICommentsPost)
 	http.ListenAndServe(Port, router)
 }
