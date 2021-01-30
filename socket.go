065b5e5f730c6c90da4c840be175c313ca68bf70
gaurang98671 <56935661+gaurang98671@users.noreply.github.com>
2021-01-30 20:10:13 +0530

    socket added

---
+++ socket.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func reader(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		log.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+		log.Println("message sent")
+	}
+
+}
+
+func serveSocket(w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	ws, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println("Client connected")
+	reader(ws)
+}
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "home")
+}
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", serveHome)
+	router.HandleFunc("/ws", serveSocket)
+	http.ListenAndServe(":8080", router)
+}
