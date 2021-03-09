b95c0830a21577155e5c163b096cd19e1bf0fba3
gaurang98671 <56935661+gaurang98671@users.noreply.github.com>
2021-03-09 11:58:23 +0530

    .

--- wait-group.go
+++ wait-group.go
@@ -3,16 +3,26 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 func process(wg *sync.WaitGroup) {
 	fmt.Println("Executing go routine")
 	wg.Done()
 }
+
+func process2(wg *sync.WaitGroup) {
+	time.Sleep(time.Duration(1 * time.Second))
+	fmt.Println("Executing process 2")
+	wg.Done()
+
+}
 func main() {
 	var wg sync.WaitGroup
-	wg.Add(2)
+	wg.Add(4)
 
+	go process2(&wg)
+	go process2(&wg)
 	go process(&wg)
 	go process(&wg)
 
